a5cb31d0e8efebec96989ba023011f7e32873657
Robert <robert.ntisthlm@gmail.com>
2023-05-03 21:07:58 +0200

    made a style and some logic for a deletenotebutton

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Image } from "react-native";
 
 export default function App() {
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handlePressIn = () => {
+    setIsHovered(true);
+  };
+
+  const handlePressOut = () => {
+    setIsHovered(false);
+  };
+
+  const handlePress = () => {
+    // Handle button press event
+    console.log("Button Pressed!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageButton
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        onPress={handlePress}
+        source={require("./icons/trashicon.png")}
+        isHovered={isHovered}
+      />
     </View>
   );
 }
+
+const ImageButton = ({ onPress, onPressIn, onPressOut, source, isHovered }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      style={[styles.button, isHovered && styles.hoveredButton]}
+      activeOpacity={1} // Set activeOpacity to 1
+    >
+      <Image
+        source={source}
+        style={[styles.image, isHovered && styles.hoveredImage]}
+      />
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +53,23 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    width: 100,
+    height: 100,
+  },
+  hoveredButton: {
+    backgroundColor: "white",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    // Add any other desired styles
+  },
+  hoveredImage: {
+    tintColor: "red",
   },
 });
