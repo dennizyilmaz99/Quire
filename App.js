deb20677553fa57dd8e4432e58f39fcceb55a20b
Robert <robert.ntisthlm@gmail.com>
2023-05-04 10:05:27 +0200

    added a caomponents folder with my deletenotebutton and then called it in app.js

--- App.js
+++ App.js
@@ -1,51 +1,14 @@
 import React from "react";
-import { StyleSheet, View, TouchableOpacity, Image } from "react-native";
+import { StyleSheet, View } from "react-native";
+import Deletenotebutton from "./components/Deletenotebutton";
 
 export default function App() {
-  const [isHovered, setIsHovered] = React.useState(false);
-
-  const handlePressIn = () => {
-    setIsHovered(true);
-  };
-
-  const handlePressOut = () => {
-    setIsHovered(false);
-  };
-
-  const handlePress = () => {
-    // Handle button press event
-    console.log("Button Pressed!");
-  };
-
   return (
     <View style={styles.container}>
-      <ImageButton
-        onPressIn={handlePressIn}
-        onPressOut={handlePressOut}
-        onPress={handlePress}
-        source={require("./icons/trashicon.png")}
-        isHovered={isHovered}
-      />
+      <Deletenotebutton></Deletenotebutton>
     </View>
   );
 }
-
-const ImageButton = ({ onPress, onPressIn, onPressOut, source, isHovered }) => {
-  return (
-    <TouchableOpacity
-      onPress={onPress}
-      onPressIn={onPressIn}
-      onPressOut={onPressOut}
-      style={[styles.button, isHovered && styles.hoveredButton]}
-      activeOpacity={1} // Set activeOpacity to 1
-    >
-      <Image
-        source={source}
-        style={[styles.image, isHovered && styles.hoveredImage]}
-      />
-    </TouchableOpacity>
-  );
-};
 
 const styles = StyleSheet.create({
   container: {
@@ -53,23 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  button: {
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "white",
-    width: 100,
-    height: 100,
-  },
-  hoveredButton: {
-    backgroundColor: "white",
-  },
-  image: {
-    width: 100,
-    height: 100,
-    // Add any other desired styles
-  },
-  hoveredImage: {
-    tintColor: "red",
   },
 });
---
+++ components/Deletenotebutton.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { StyleSheet, View, TouchableOpacity, Image } from "react-native";
+
+export default function Deletenotebutton() {
+  const [isHovered, setIsHovered] = React.useState(false);
+
+  const handlePressIn = () => {
+    setIsHovered(true);
+  };
+
+  const handlePressOut = () => {
+    setIsHovered(false);
+  };
+
+  const handlePress = () => {
+    // Handle button press event
+    console.log("Button Pressed!");
+  };
+
+  return (
+    <View style={styles.container}>
+      <ImageButton
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        onPress={handlePress}
+        source={require("../icons/trashicon.png")}
+        isHovered={isHovered}
+      />
+    </View>
+  );
+}
+
+const ImageButton = ({ onPress, onPressIn, onPressOut, source, isHovered }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      onPressIn={onPressIn}
+      onPressOut={onPressOut}
+      style={[styles.button, isHovered && styles.hoveredButton]}
+      activeOpacity={1} // Set activeOpacity to 1
+    >
+      <Image
+        source={source}
+        style={[styles.image, isHovered && styles.hoveredImage]}
+      />
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+    width: 50,
+    height: 50,
+  },
+  hoveredButton: {
+    backgroundColor: "white",
+  },
+  image: {
+    width: 50,
+    height: 50,
+    // Add any other desired styles
+  },
+  hoveredImage: {
+    tintColor: "red",
+  },
+});
